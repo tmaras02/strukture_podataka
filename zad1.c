10aed8f422a7fe34e038a64c3560d51325e107ce
RMartinic <115662731+RMartinic@users.noreply.github.com>
2022-10-20 21:28:27 +0200

    Update zad1.c

--- zad1.c
+++ zad1.c
@@ -24,16 +24,26 @@ void podaciStudenta(char* filename, _student*);
 int main(void)
 {
 	char filename[MAX_FILE_NAME] = { 0 };
+	_student* osoba = NULL;
+	int count = 0;
 
 	printf("Insert filename > ");
 	scanf(" %s", filename);
 
-	printf("Broj studenata u datoteci %s je %d", filename, countStudentsFromFile(filename));
+	count = countStudentsFromFile(filename);
 
-	_student* osoba;
-	osoba = malloc(countStudentsFromFile(filename) * sizeof(_student));
 
-	podaciStudenta(filename, osoba);
+	if (count > 0) {
+		printf("Broj studenata u datoteci %s je %d", filename, count);
+		osoba = malloc(count * sizeof(_student));
+
+		if (!osoba) {
+			perror("Memory allocation failed for students!");
+			return EXIT_FAILURE;
+		}
+
+		podaciStudenta(filename, osoba, count);
+	}
 
 	return 0;
 }
@@ -42,7 +52,9 @@ int countStudentsFromFile(char* filename)
 {
 	FILE* fp = NULL;
 	int count = 0;
-	char buffer[MAX_LINE] = { 0 };
+	char ime[MAX_NAME] = { 0 };
+	char prezime[MAX_NAME] = { 0 };
+	int bodovi = 0;
 
 	fp = fopen(filename, "r");
 	if (fp == NULL)
@@ -53,9 +65,8 @@ int countStudentsFromFile(char* filename)
 
 	while (!feof(fp))
 	{ // while not found end of file (EOF)
-		fgets(buffer, MAX_LINE, fp);
-		if (strcmp("\n", buffer) != 0)
-		{
+		int result = fscanf(fp, " %s %s %d", ime, prezime, &bodovi);
+		if (result == 3) {
 			count++;
 		}
 	}
@@ -64,29 +75,30 @@ int countStudentsFromFile(char* filename)
 	return count;
 }
 
-void podaciStudenta(char* filename, _student* osoba)
+int podaciStudenta(char* filename, _student* osoba, int count)
 {
 	FILE* fp = NULL;
 	char buffer[MAX_LINE] = { 0 };
+	int i = 0;
 
 	fp = fopen(filename, "r");
 	if (fp == NULL)
 	{
 		printf("Dear customer, the file %s didn't open!\r\n", filename);
+		return EXIT_FAILURE;
 	}
 
-	int i = 0;
 
 	while (!feof(fp))
 	{ // while not found end of file (EOF)
-		fscanf(fp, "%s %s %d", osoba[i].ime, osoba[i].prezime, &osoba[i].bodovi);
+		fscanf(fp, " %s %s %d", osoba[i].ime, osoba[i].prezime, &osoba[i].bodovi);
 		osoba[i].relativan_br_bodova = (((float)osoba[i].bodovi) / MAX_BOD) * 100;
 		i++;
 	}
 
 	fclose(fp);
 
-	for (int j = 0; j < countStudentsFromFile(filename); j++)
+	for (int j = 0; j < count; j++)
 	{
 		printf("\n%s %s %d %d", osoba[j].ime, osoba[j].prezime, osoba[j].bodovi, osoba[j].relativan_br_bodova);
 	}
