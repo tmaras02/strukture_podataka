586d882a626fb8c5894f183d00f3ccd5202fdd88
Tomislav Maras <115408239+tmaras02@users.noreply.github.com>
2022-11-17 19:08:43 +0100

    Update Source.c

--- Zad5Struk/Zad5Struk/Source.c
+++ Zad5Struk/Zad5Struk/Source.c
@@ -174,15 +174,3 @@ int check() {
 		return 0;
 	}
 }
-
-int Delete() {
-	position head = top->next;
-	position temp;
-
-	while (head != NULL) {
-		temp = head->next;
-		free(head);
-		head = temp;
-	}
-	return 0;
-}
